a2dd939c4eec10378a003f56bb6552c26c1c6265
Shajed <ctgboy2010@gmail.com>
2014-12-18 05:43:10 +0600

    htmlentities

---
+++ string/htmlentities.php
@@ -0,0 +1,10 @@
+<?php
+    //Example 1: a htmlentities()
+    $str = "A 'quote' is <b>bold</b>";
+
+    // Outputs: A 'quote' is &lt;b&gt;bold&lt;/b&gt;
+    echo htmlentities($str);
+
+    // Outputs: A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt;
+    echo htmlentities($str, ENT_QUOTES);
+?>
