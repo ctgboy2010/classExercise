091b36c32cce84c2bcaca155809601000d71de65
shajed <ctgboy2010@gmail.com>
2014-12-19 22:23:29 +0600

    array key exists function

---
+++ array/array_key_exists.php
@@ -0,0 +1,8 @@
+<?php
+    //Example 1: array_key_exists
+
+    $a=array('first'=>1,'second'=>4);
+    if(array_key_exists('first', $a)){
+        echo "The 'first' element is in the array";
+    }
+?>
