f98bfab259268b7747e3e5d430cb3c0338598509
shajed <ctgboy2010@gmail.com>
2014-12-19 22:13:26 +0600

    array difference function

---
+++ array/array_diff.php
@@ -0,0 +1,11 @@
+<?php
+    //Example 1: array_diff()
+
+    $a=array('a'=>'green','red',23=>'blue','sky','red','sky');
+    $b=array('a'=>'b','yellow',23=>'red');
+    $c=array('a'=>'orange','think','chokolet','deep','sky');
+    print_r($a);
+    print_r($b);
+    print_r($c);
+    print_r(array_diff($a,$b,$c));
+?>
